d8b5cdfc05f374c676397b5a1d488e42a109ad53
anasa <anas-aji-seeroo>
2022-10-26 17:56:01 +0100

    Project Created

---
+++ app/src/main/java/com/uog_mobile_application_development/m_hike/MainActivity.java
@@ -0,0 +1,14 @@
+package com.uog_mobile_application_development.m_hike;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
