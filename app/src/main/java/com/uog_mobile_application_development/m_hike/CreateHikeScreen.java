695e97f5e4e0ac6d30ca463f1888c86bf1f554a5
anasa <anas-aji-seeroo>
2022-10-27 17:20:01 +0100

    Getting started page, home page file and create hike page added.

---
+++ app/src/main/java/com/uog_mobile_application_development/m_hike/CreateHikeScreen.java
@@ -0,0 +1,14 @@
+package com.uog_mobile_application_development.m_hike;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateHikeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_hike_screen);
+    }
+}
---
+++ app/src/main/java/com/uog_mobile_application_development/m_hike/HomeScreen.java
@@ -0,0 +1,14 @@
+package com.uog_mobile_application_development.m_hike;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
